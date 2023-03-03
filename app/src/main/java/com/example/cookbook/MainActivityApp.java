90ebe8cc4dfe9687df962df9710759dc5d20a1eb
HadarBasson1 <hadarbsn@gmail.com>
2023-03-03 23:55:27 +0200

    action bar

--- app/src/main/java/com/example/cookbook/MainActivityApp.java
+++ app/src/main/java/com/example/cookbook/MainActivityApp.java
@@ -1,14 +1,29 @@
 package com.example.cookbook;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.widget.Toolbar;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivityApp extends AppCompatActivity {
-
+   NavController navController;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_app);
+
+//
+//
+        NavHostFragment navHostFragment = (NavHostFragment)getSupportFragmentManager().findFragmentById(R.id.main_navhost);
+        navController = navHostFragment.getNavController();
+        NavigationUI.setupActionBarWithNavController(this,navController);
+
+//        BottomNavigationView navView = findViewById(R.id.main_bottomNavigationView);
+//        NavigationUI.setupWithNavController(navView,navController);
     }
 }
---
+++ app/src/main/java/com/example/cookbook/model/Recipe.java
@@ -0,0 +1,7 @@
+package com.example.cookbook.model;
+
+
+
+public class Recipe {
+
+}
