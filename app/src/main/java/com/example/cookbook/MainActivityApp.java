b46805c44dfb327fc37dddf70002a74adedd3299
HadarBasson1 <hadarbsn@gmail.com>
2023-03-04 11:28:34 +0200

    action bar

--- app/src/main/java/com/example/cookbook/MainActivityApp.java
+++ app/src/main/java/com/example/cookbook/MainActivityApp.java
@@ -1,11 +1,13 @@
 package com.example.cookbook;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.Toolbar;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -25,5 +27,19 @@ public class MainActivityApp extends AppCompatActivity {
 
 //        BottomNavigationView navView = findViewById(R.id.main_bottomNavigationView);
 //        NavigationUI.setupWithNavController(navView,navController);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (item.getItemId() == android.R.id.home) {
+            navController.popBackStack();
+        }
+//        else{
+//            fragmentMenuId = item.getItemId();
+//            return NavigationUI.onNavDestinationSelected(item,navController);
+//        }
+        return super.onOptionsItemSelected(item);
     }
 }
