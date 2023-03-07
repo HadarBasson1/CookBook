b06fa79f40f99b475d653b6b9a8f79f6c33c9f97
Noam Lushi <noamlushi@gmail.com>
2023-03-07 17:32:25 +0200

    register

--- app/src/main/java/com/example/cookbook/Register.java
+++ app/src/main/java/com/example/cookbook/Register.java
@@ -1,14 +1,82 @@
 package com.example.cookbook;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Register extends AppCompatActivity {
+        EditText editTextEmail,editTextPassword;
+        Button buttonReg;
+        FirebaseAuth mAuth;
+        ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        mAuth = FirebaseAuth.getInstance();
+        editTextEmail=findViewById(R.id.register_email);
+        editTextPassword=findViewById(R.id.regidter_password);
+        buttonReg=findViewById(R.id.register_btn);
+        progressBar = findViewById(R.id.register_progressbar);
+
+
+
+
+        buttonReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                progressBar.setVisibility(View.VISIBLE);
+                String email, password;
+                email=String.valueOf(editTextEmail.getText());
+                password=String.valueOf(editTextPassword.getText());
+
+
+                if(TextUtils.isEmpty(email)){
+                    Toast.makeText(Register.this,"Enter Email",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(TextUtils.isEmpty(password)){
+                    Toast.makeText(Register.this,"Enter Password",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+
+
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener( new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                progressBar.setVisibility(View.GONE);
+
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(Register.this, "Account created.",
+                                            Toast.LENGTH_SHORT).show();
+
+//                                    // Sign in success, update UI with the signed-in user's information
+//                                    FirebaseUser user = mAuth.getCurrentUser();
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Toast.makeText(Register.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+
+            }
+        });
     }
 }
